8ec58eba91dd2b0ad244c78d25b6e502d6270585
unknown <xarvichanx@gmail.com>
2024-07-02 20:24:28 +0200

    logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,18 @@
 
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+# Tworzenie 100 plików logx.txt
+for i in {1..100}
+do
+    filename="log${i}.txt"
+    current_date=$(date)
+    # Zapisanie informacji do pliku
+    echo "Nazwa pliku: $filename" > $filename
+    echo "Nazwa skryptu: $0" >> $filename
+    echo "Data: $current_date" >> $filename
+done
+echo "Utworzono 100 plików logx.txt"
 else
     echo "Wrong arguments"
 fi
