3564deb12cf4f78ec59a2a181713a6086b4509c4
unknown <xarvichanx@gmail.com>
2024-07-05 22:00:42 +0200

    added single flags

--- skrypt.sh
+++ skrypt.sh
@@ -18,6 +18,10 @@ elif [ "$1" == "--logs" ]; then
         echo "Data: $current_date" >> $filename
     done
     echo "Utworzono $num_files plików logx.txt"
+elif [ "$1" == "--help" ]; then
+    echo "--date wyświetli aktualną datę"
+    echo "--logs utworzy 100 plików logx.txt"
+    echo "--logs <number> utworzy daną liczbę plików log.txt"
 else
     echo "Nieprawidłowe argumenty"
 fi
